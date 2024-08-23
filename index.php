0a2fdef60325ad059ffffc10714ca258b0230142
MiguelCosta07 <miguelestudos0405@gmail.com>
2024-08-23 14:12:47 -0300

    versão com imagem ruim

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilo.css"> <!-- Link para o arquivo CSS -->
+    <title>SportQuiz</title>
+</head>
+<body>
+</body>
+</html>
