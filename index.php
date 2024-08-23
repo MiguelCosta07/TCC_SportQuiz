b97c01972089a2a17e603ecdb5652a81d6ced5f2
Miguel Costa <miguelestudos0405@gmail.com>
2024-08-23 19:01:22 -0300

    tops

--- index.php
+++ index.php
@@ -1,11 +1,18 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
+    <div>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="estilo.css"> <!-- Link para o arquivo CSS -->
     <title>SportQuiz</title>
+    <img class="logo" alt="imagemquiz" src="/imagem/testequiz.jpg">
+    <h1>SportQuiz</h1>
+    <h3>Cadastrar</h3>
+    <h3 class="login">Login</h3>
+    </div>
 </head>
 <body>
+
 </body>
 </html>
